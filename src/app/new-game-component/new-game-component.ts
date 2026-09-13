566c2c987be8b52743239d66f437e9b9fa6a29bd
Giulio <giulioweiss65@outlook.de>
2026-09-13 21:32:40 +0200

    add new-game-component

--- src/app/new-game-component/new-game-component.ts
+++ src/app/new-game-component/new-game-component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-new-game-component',
   styleUrl: './new-game-component.css',
   templateUrl: './new-game-component.html',
 })
-export class NewGameComponent {}
+export class NewGameComponent {
+    router = inject(Router);
+
+    mode: 'bot' | 'local' = 'bot';
+    botCount = 1;
+
+    start(): void {
+        this.router.navigate(['/game'], {
+            queryParams: {
+                mode: this.mode,
+                bots: this.botCount
+            }
+        });
+    }
+
+    back(): void {
+        this.router.navigateByUrl('/home');
+    }
+}
+
